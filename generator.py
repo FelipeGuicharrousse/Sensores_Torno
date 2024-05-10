8bb53895fc75b2937507c314aabb8135c67e62af
FelipeGuicharrousse <felipe.guicharrousse@usm.cl>
2024-05-10 16:37:18 -0400

    🚧 New format for excel

--- generator.py
+++ generator.py
@@ -1,6 +1,6 @@
 import pandas as pd
 from motor.motor_asyncio import AsyncIOMotorClient
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 # Conexión a la base de datos MongoDB
@@ -15,68 +15,75 @@ SENSOR_COLUMNS = {
     "Sensor 3": ["Velocidad (RPM)"]
 }
 
-async def generate_excel_for_date(selected_date):
-    # Formatear la fecha seleccionada para que coincida con el formato de la base de datos
-    selected_date_str = selected_date.strftime('%Y-%m-%d')
-    next_day = (selected_date + pd.Timedelta(days=1)).strftime('%Y-%m-%d')
+
+fecha_inicial = datetime(2024, 5, 1) 
+fecha_final = datetime.now()  
+
+async def generate_excel_for_date():
     
     # Crear un archivo Excel con el nombre basado en la fecha seleccionada
-    file_name = f"sensor_data_{selected_date_str}.xlsx"
+    file_name = f"sensor_data.xlsx"
     with pd.ExcelWriter(file_name, engine='xlsxwriter') as writer:
         for sensor_info in SENSOR_INFO:
             sensor_name = sensor_info["name"]
             collection_name = sensor_info["collection"]
             collection = db[collection_name]
             
-            # Consultar los datos del sensor solo para la fecha seleccionada
-            sensor_data = []
-            cursor = collection.find({"timestamp": {"$gte": datetime.combine(selected_date, datetime.min.time()), "$lt": datetime.combine(selected_date + pd.Timedelta(days=1), datetime.min.time())}})
-            async for document in cursor:
-                sensor_data.append(document)
-            
-            if not sensor_data:
-                print(f"No hay datos disponibles para el sensor {sensor_name} en la fecha seleccionada.")
-                continue
-            
-            # Crear un DataFrame de Pandas con los datos del sensor
-            df = pd.DataFrame(sensor_data)
-            
-            # Crear columnas para cada sensor según el orden definido
-            if sensor_name in SENSOR_COLUMNS:
-                columns_order = SENSOR_COLUMNS[sensor_name]
-                timestamp_column = df['timestamp']
-                df = pd.DataFrame(df["data"].tolist(), columns=columns_order)
+            fecha_actual = fecha_inicial    
+            df_final = pd.DataFrame()
+            while fecha_actual <= fecha_final:
+                # Consultar los datos del sensor solo para la fecha seleccionada
+                sensor_data = []
+                cursor = collection.find({"timestamp": {"$gte": datetime.combine(fecha_actual, datetime.min.time()), "$lt": datetime.combine(fecha_actual + pd.Timedelta(days=1), datetime.min.time())}})
+                async for document in cursor:
+                    sensor_data.append(document)
                 
-                # Convertir los valores a números si es posible
-                for column in df.columns:
-                    df[column] = pd.to_numeric(df[column])
-                df.insert(0, 'Timestamp', timestamp_column)
+                if not sensor_data:
+                    print(f"No hay datos disponibles para el sensor {sensor_name} en la fecha seleccionada.")
+                    continue
                 
-                # Calcular el promedio, máximo y mínimo para cada columna numérica
-                stats = {}
-                for column in df.columns:
-                    if pd.api.types.is_numeric_dtype(df[column]):
-                        stats[column] = {
-                            'Promedio': df[column].mean(),
-                            'Máximo': df[column].max(),
-                            'Mínimo': df[column].min()
-                        }
+                # Crear un DataFrame de Pandas con los datos del sensor
+                df = pd.DataFrame(sensor_data)
                 
-                # Crear un DataFrame para las filas 'Promedio', 'Máximo' y 'Mínimo'
-                stats_df = pd.DataFrame(stats)
-                stats_df.index = ['Promedio', 'Máximo', 'Mínimo']
-                stats_df = stats_df.reset_index()
-                stats_df = stats_df.rename(columns={'index': 'Timestamp'})
+                # Crear columnas para cada sensor según el orden definido
+                if sensor_name in SENSOR_COLUMNS:
+                    columns_order = SENSOR_COLUMNS[sensor_name]
+                    timestamp_column = df['timestamp']
+                    df = pd.DataFrame(df["data"].tolist(), columns=columns_order)
+                    
+                    # Convertir los valores a números si es posible
+                    for column in df.columns:
+                        df[column] = pd.to_numeric(df[column])
+                    df.insert(0, 'Timestamp', timestamp_column)
+                    
+                    # Calcular el promedio, máximo y mínimo para cada columna numérica
+                    stats = {}
+                    for column in df.columns:
+                        if pd.api.types.is_numeric_dtype(df[column]):
+                            stats[column] = {
+                                'Promedio': df[column].mean(),
+                                'Máximo': df[column].max(),
+                                'Mínimo': df[column].min()
+                            }
+                    
+                    # Crear un DataFrame para las filas 'Promedio', 'Máximo' y 'Mínimo'
+                    stats_df = pd.DataFrame(stats)
+                    stats_df.index = ['Promedio', 'Máximo', 'Mínimo']
+                    stats_df = stats_df.reset_index()
+                    stats_df = stats_df.rename(columns={'index': 'Timestamp'})
+                    
+                    # Agregar las filas al final del DataFrame del sensor
+                    df_final = pd.concat([df_final, stats_df], ignore_index=True)
+
+                 
+                else:
+                    print(f"No se encontró el orden de columnas para el sensor {sensor_name}.")
+                    continue
+
+                fecha_actual += timedelta(days=1) 
                 
-                # Agregar las filas al final del DataFrame del sensor
-                df = pd.concat([df, stats_df], ignore_index=True)
-                
-            else:
-                print(f"No se encontró el orden de columnas para el sensor {sensor_name}.")
-                continue
-            
             # Escribir los datos en una hoja de Excel con el nombre del sensor
-            df.to_excel(writer, sheet_name=sensor_name, index=False)
+            df_final.to_excel(writer, sheet_name=sensor_name, index=False)
             
             print(f"Datos del sensor {sensor_name} guardados en la hoja '{sensor_name}' del archivo Excel.")
     
@@ -88,13 +95,9 @@ if __name__ == "__main__":
     # Configurar la información de los sensores
     SENSOR_INFO = [
         {"port": 5000, "name": "Torno 6", "collection": "torno_6_data"},
-        {"port": 5500, "name": "Humedad_Temperatura", "collection": "hum_temp_data"},
+        {"port": 5500, "name": "Hum_Temp", "collection": "hum_temp_data"},
         {"port": 6000, "name": "Torno 8", "collection": "torno_8_data"}
     ]
 
-    # Solicitar al usuario la fecha deseada para generar el archivo Excel
-    input_date = input("Ingrese la fecha en formato YYYY-MM-DD: ")
-    selected_date = datetime.strptime(input_date, "%Y-%m-%d")
-
     loop = asyncio.get_event_loop()
-    loop.run_until_complete(generate_excel_for_date(selected_date))
+    loop.run_until_complete(generate_excel_for_date())
